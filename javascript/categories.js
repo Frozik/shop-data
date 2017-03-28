47713477ca75d5895dada45ee37babbf35b75217
Dmitry Sharov <dsharov@griddynamics.com>
2017-03-28 10:47:51 +0300

    Added shop data

---
+++ javascript/categories.js
@@ -0,0 +1,22 @@
+const sourceCategories = [
+    {
+        id: 0,
+        name: 'Active wear',
+    },
+    {
+        id: 1,
+        name: 'Jeans',
+    },
+    {
+        id: 2,
+        name: 'Coats',
+    },
+    {
+        id: 3,
+        name: 'Sweaters',
+    },
+    {
+        id: 4,
+        name: 'Wear to work',
+    },
+];
---
+++ typescript/categories.ts
@@ -0,0 +1,27 @@
+interface ICategory {
+    readonly id: number;
+    readonly name: string;
+}
+
+const sourceCategories: ICategory[] = [
+    {
+        id: 0,
+        name: 'Active wear',
+    },
+    {
+        id: 1,
+        name: 'Jeans',
+    },
+    {
+        id: 2,
+        name: 'Coats',
+    },
+    {
+        id: 3,
+        name: 'Sweaters',
+    },
+    {
+        id: 4,
+        name: 'Wear to work',
+    },
+];
